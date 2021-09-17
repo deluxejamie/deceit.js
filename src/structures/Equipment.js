fdec2ad65360a4076822d65d38b21978d6ba90c5
MainLeau <mail@mainleau.com>
2021-09-17 23:45:07 +0200

    fix(Loadout): set default equipement to -1

--- src/structures/Equipment.js
+++ src/structures/Equipment.js
@@ -2,15 +2,15 @@ const Item = require('./Item');
 
 class Equipment {
   constructor(data) {
-    this.hair = new Item({ id: data[0] });
-    this.accessory = new Item({ id: data[1] });
-    this.clothes = new Item({ id: data[2] });
-    this.wristband = new Item({ id: data[3] });
-    this.pistol = new Item({ id: data[4] });
-    this.knife = new Item({ id: data[5] });
-    this.victoryPose = new Item({ id: data[6] });
-    this.defeatPose = new Item({ id: data[7] });
-    this.spray = new Item({ id: data[8] });
+    this.hair = data[0] !== -1 ? new Item({ id: data[0] }) : -1;
+    this.accessory = data[1] !== -1 ? new Item({ id: data[1] }) : -1;
+    this.clothes = data[2] !== -1 ? new Item({ id: data[2] }) : -1;
+    this.wristband = data[3] !== -1 ? new Item({ id: data[3] }) : -1;
+    this.pistol = data[4] !== -1 ? new Item({ id: data[4] }) : -1;
+    this.knife = data[5] !== -1 ? new Item({ id: data[5] }) : -1;
+    this.victoryPose = data[6] !== -1 ? new Item({ id: data[6] }) : -1;
+    this.defeatPose = data[7] !== -1 ? new Item({ id: data[7] }) : -1;
+    this.spray = data[8] !== -1 ? new Item({ id: data[8] }) : -1;
   }
 }
 
