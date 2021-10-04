21297014659314652c184dcfd4899ac500f2da6a
MainLeau <mail@mainleau.com>
2021-10-04 13:03:26 +0200

    feat(ELOHighscore): add 'defaultELO' getter

--- src/structures/ELOHighscore.js
+++ src/structures/ELOHighscore.js
@@ -4,6 +4,11 @@ class ELOHighscore extends Highscore {
   constructor(data, Highscores) {
     super(data, Highscores);
   }
+
+  get defaultELO() {
+    return 1200;
+  }
+
 }
 
 module.exports = ELOHighscore;
